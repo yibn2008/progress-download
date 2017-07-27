e29abc39bed8a3703d155b2c2f63af9cb536dd28
yibn2008 <yibn2008@qq.com>
2017-07-27 17:24:28 +0800

    fix: fix: fix download 5.x data event bug

--- test/index.test.js
+++ test/index.test.js
@@ -4,6 +4,7 @@ const fs = require('fs')
 const rimraf = require('rimraf')
 const path = require('path')
 const assert = require('assert')
+const cp = require('child_process')
 const download = require('..')
 
 describe('test progress download', function () {
@@ -20,6 +21,10 @@ describe('test progress download', function () {
   })
 
   it('should download with progress bar', async function () {
-    await download('https://codeload.github.com/yibn2008/fast-sass-loader/zip/master', cacheDir)
+    await download('https://github.com/yibn2008/progress-download/archive/master.zip', cacheDir)
+
+    cp.execSync('tar -zxf master.zip', {
+      cwd: cacheDir
+    })
   })
 })
